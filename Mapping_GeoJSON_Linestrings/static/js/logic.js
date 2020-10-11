215d4ce52a449d6cd77d667f0f5236b5d05f4b05
Greg Sherman <6807242+gregoryhsherman@users.noreply.github.com>
2020-10-11 15:49:55 -0400

    message

---
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -0,0 +1,22 @@
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://github.com/gregoryhsherman/mapping_earthquakes/blob/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
