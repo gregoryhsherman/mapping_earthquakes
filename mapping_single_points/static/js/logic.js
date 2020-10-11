5f15595903fb00f8bd061234d4f00c151eca02bf
Greg Sherman <6807242+gregoryhsherman@users.noreply.github.com>
2020-10-11 15:09:22 -0400

    message

--- mapping_single_points/static/js/logic.js
+++ mapping_single_points/static/js/logic.js
@@ -6,7 +6,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 //  Add a marker to the map for Los Angeles, California.
 L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
+    radius: 200,
     color: 'black',
     fillColor: '#ffffa1'
  }).addTo(map);
